9fdc9b35d4fbf5c2b1e5caefe9dab25c71da9cab
rekkun <reky.rolen@gmail.com>
2019-07-17 05:25:14 +0700

    refactor ds_array

--- dataStructure/ds_array.py
+++ dataStructure/ds_array.py
@@ -1,22 +1,30 @@
 from array import *
 
-#create array
-array1 = array('i', [10,20,30,40,50])
-print("Original Array")
-for x in array1:
-    print(x)
 
-#delete operation
+def print_array(array):
+    for x in array:
+        print(x)
+
+
+# create array
+array1 = array('i', [10, 20, 30, 40, 50])
+print("Original Array")
+print_array(array1)
+
+# delete operation
 array1.remove(50)
 print("\n50 remove from array")
-for x in array1:
-    print(x)
+print_array(array1)
 
-#search operation
+# search operation
 print("\nSearch index for value 30 in array: ", array1.index(30))
 
-#update operation
+# update operation
 array1[1] = 21
 print("\nupdate value on index 1 in array")
-for x in array1:
-    print(x)
+print_array(array1)
+
+# insert operation
+print("\nadd 60 to index 4 to array")
+array1.insert(4, 60)
+print_array(array1)
