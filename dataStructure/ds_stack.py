f816f9468c2848398d47ae3beeef58eda0bdcd9e
Rekkun <reky.rolen@gmail.com>
2019-07-21 10:13:47 +0700

    Add stack data structure

---
+++ dataStructure/ds_stack.py
@@ -0,0 +1,30 @@
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def add(self, data):
+
+        if data not in self.stack:
+            self.stack.append(data)
+            return True
+        else:
+            return False
+
+    def peek(self):
+        return self.stack[-1]
+
+    def remove(self):
+        if len(self.stack) <= 0:
+            return("No element in the Stack")
+        else:
+            return self.stack.pop()
+
+AStack = Stack()
+AStack.add("Mon")
+AStack.add("Tue")
+AStack.add("Wed")
+AStack.add("Thu")
+print(AStack.peek())
+print("item will remove ->", AStack.remove())
+print(AStack.peek())
